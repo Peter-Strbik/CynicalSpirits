650db1a5cdbc8740c394b833564141c212a299f5
Albert Mokrejs <albertmokrejs@gmail.com>
2016-04-21 13:11:50 -0400

    Update bouncy.js

--- bouncy.js
+++ bouncy.js
@@ -75,7 +75,7 @@ var makeBall = function(){
 
 
 
-var distBetween = function(ball1, ball2){
+var distanceBetween = function(ball1, ball2){
 	return Math.pow((ball1.x - ball2.x),2) + Math.pow((ball1.y - ball2.y),2) <= Math.pow((ball1.r + ball2.r),2);
 };
 
